bfe98caa119a9ec4ed0e491ed6a81dbfe15217e3
Kyle Szombathy <kszombathy@riskalyze.com>
2018-05-20 21:58:15 -0700

    Change book a room

--- resources/views/emails/rsvp/thankyou.blade.php
+++ resources/views/emails/rsvp/thankyou.blade.php
@@ -7,7 +7,7 @@ Here are some links you might find useful:
 
 We have reserved a block of rooms at the Residence Inn Marriott at Capitol Park. Book by July 5th to receive the special rate.
 
-@component('mail::button', ['url' => 'https://bit.ly/2LhKkVB'])
+@component('mail::button', ['url' => 'https://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Noelle%20and%20Kyle%27s%20Wedding^SACDT`NKWNKWA|NKWNKWC`150.00-270.00`USD`false`6`8/3/18`8/5/18`7/5/18&app=resvlink&stop_mobi=yes'])
     Book a room
 @endcomponent
 
