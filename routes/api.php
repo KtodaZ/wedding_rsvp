7eec6eb204b0849860a25eaafdf6cade74d6c3e5
Kyle Szombathy <kszombathy@riskalyze.com>
2018-07-04 10:45:22 -0700

    Remove test email endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,3 @@
 
 Route::resource('attendee', '\App\Http\Controllers\Api\AttendeeController', ['only' => ['show', 'store']])
     ->middleware('auth');
-
-Route::get('send_test_email', function () {
-    Mail::raw('Sending emails with Mailgun and Laravel is easy!', function ($message) {
-        $message->to('kylemail2003@gmail.com');
-    });
-});
